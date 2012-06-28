8de29bd79933c6b5c1b9d3d95f91b1bd5db30fe2
Anton Pawlik <apaw@kupferwerk.net>
2012-06-28 13:42:00 +0200

    missing i18n stuff

--- app/helpers/bhf/application_helper.rb
+++ app/helpers/bhf/application_helper.rb
@@ -2,19 +2,19 @@ module Bhf
   module ApplicationHelper
 
     def show_t(platform)
-      t("bhf.helpers.entry.models.#{platform.model_name}.show", platform_name: platform.title_singular, default: t('bhf.helpers.entry.show'))
+      t("bhf.helpers.entry.models.#{platform.model_name}.show", platform_name: platform.title_singular, default: t('bhf.helpers.entry.show')).html_safe
     end
 
     def new_t(platform)
-      t("bhf.helpers.entry.models.#{platform.model_name}.new", platform_name: platform.title_singular, default: t('bhf.helpers.entry.new'))
+      t("bhf.helpers.entry.models.#{platform.model_name}.new", platform_name: platform.title_singular, default: t('bhf.helpers.entry.new')).html_safe
     end
 
     def edit_t(platform)
-      t("bhf.helpers.entry.models.#{platform.model_name}.edit", platform_name: platform.title_singular, default: t('bhf.helpers.entry.edit'))
+      t("bhf.helpers.entry.models.#{platform.model_name}.edit", platform_name: platform.title_singular, default: t('bhf.helpers.entry.edit')).html_safe
     end
 
     def delete_t(platform)
-      t("bhf.helpers.entry.models.#{platform.model_name}.delete", platform_name: platform.title_singular, default: t('bhf.helpers.entry.delete'))
+      t("bhf.helpers.entry.models.#{platform.model_name}.delete", platform_name: platform.title_singular, default: t('bhf.helpers.entry.delete')).html_safe
     end
 
   end
