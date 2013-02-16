fa56f83a3c059087425f6aca60aa7c5099061890
Anton Pawlik <apaw@kupferwerk.net>
2013-02-17 00:33:18 +0100

    cosmetic changes

--- Rakefile
+++ Rakefile
@@ -11,12 +11,12 @@ begin
 
   Jeweler::Tasks.new do |gem|
     gem.name = 'bhf'
-    gem.summary = 'Agnostic rails backend'
+    gem.summary = 'Agnostic Rails backend'
     gem.description = 'Gets you there on time'
     gem.email = 'anton.pawlik@gmail.com'
     gem.authors = ['Anton Pawlik']
     gem.files = Dir["{lib}/**/*", "{app}/**/*", "{config}/**/*", "vendor/assets/**/*"]
-    gem.homepage = 'http://github.com/antpaw/bahnhof'
+    gem.homepage = 'http://github.com/antpaw/bhf'
     gem.rubyforge_project = 'nowarning'
     gem.add_dependency 'rails', '>= 3.2.0'
     gem.add_dependency 'kaminari', '>= 0.12.4'
--- lib/bhf.rb
+++ lib/bhf.rb
@@ -1,8 +1,49 @@
 def d(var)
   raise var.inspect.split(', ').join(",\n")
 end
+require 'haml'
+require 'kaminari'
 
-require 'engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
+module Bhf
+  class Engine < Rails::Engine
+
+    # Config defaults
+    config.page_title = nil
+    config.mount_at = 'bhf'
+    config.on_login_fail = :root_url
+    config.logout_path = :logout_path
+    config.session_auth_name = :is_admin
+    config.session_account_id = :admin_account_id
+    config.account_model = 'User'
+    config.account_model_find_method = 'find'
+    config.css = []
+    
+    config.routes = lambda {
+      namespace :bhf, path: Bhf::Engine.config.mount_at do
+        root to: 'application#index'
+
+        get 'page/:page', to: 'pages#show', as: :page
+
+        scope ':platform' do
+          resources :entries, except: [:index] do
+            put :sort, on: :collection
+
+            resources :embed_entries, except: [:index, :show], as: :embed
+            post :duplicate, on: :member
+          end
+        end
+
+      end
+    }
+
+    
+    config.remove_default_routes = false
+    
+    # config.bhf_logic = YAML::load(IO.read('config/bhf.yml'))
+
+  end
+end
+
 require 'bhf/i18n'
 require 'bhf/active_record/active_record'
 require 'bhf/active_record/upload'
