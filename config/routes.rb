2d9f7b0f4e36718abf7ff055d187da579fa37288
Anton Pawlik <anton.pawlik@gmail.com>
2013-12-17 19:36:44 +0100

    routes config rework

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
-unless Bhf::Engine.config.remove_default_routes
-  Rails.application.routes.draw(&Bhf::Engine.config.bhf_routes)
+Bhf::Engine.routes.draw do
+  scope as: :bhf do
+    root to: 'application#index'
+    
+    get 'page/:page', to: 'pages#show', as: :page
+    
+    scope ':platform' do
+      resources :entries, except: [:index] do
+        put :sort, on: :collection
+        
+        resources :embed_entries, except: [:index, :show], as: :embed
+        post :duplicate, on: :member
+      end
+    end
+  end
+  
   #(bhf_)((.+)_?(path|url))
 end
--- lib/bhf.rb
+++ lib/bhf.rb
@@ -6,9 +6,11 @@ require 'kaminari'
 
 module Bhf
   class Engine < Rails::Engine
+    
+    isolate_namespace Bhf
+    
     # Config defaults
     config.page_title = nil
-    config.mount_at = 'bhf'
     config.on_login_fail = :root_url
     config.logout_path = :logout_path
     config.session_auth_name = :is_admin
@@ -17,27 +19,6 @@ module Bhf
     config.account_model_find_method = 'find'
     config.css = []
     config.js = []
-    
-    config.bhf_routes = lambda {
-      namespace :bhf, path: Bhf::Engine.config.mount_at do
-        root to: 'application#index'
-
-        get 'page/:page', to: 'pages#show', as: :page
-
-        scope ':platform' do
-          resources :entries, except: [:index] do
-            put :sort, on: :collection
-
-            resources :embed_entries, except: [:index, :show], as: :embed
-            post :duplicate, on: :member
-          end
-        end
-
-      end
-    }
-
-    
-    config.remove_default_routes = false
     
     # config.bhf_logic = YAML::load(IO.read('config/bhf.yml'))
 
