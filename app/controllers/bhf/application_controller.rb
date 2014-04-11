3168556c7045a440f374a85954535c5c69f38d64
Anton Pawlik <anton.pawlik@gmail.com>
2014-04-11 16:17:43 +0200

    clean up

--- app/controllers/bhf/application_controller.rb
+++ app/controllers/bhf/application_controller.rb
@@ -33,80 +33,6 @@ class Bhf::ApplicationController < ActionController::Base
       @current_account
     end
 
-    def load_config
-      roles_config = roles_yml(get_account_roles(params[:bhf_area]))
-      
-      if Bhf::Engine.config.abstract_config.any?
-        
-        abstract_platform_config = get_config_array(Bhf::Engine.config.abstract_config, '/abstract')['pages'].each_with_object({}) do |abstract_pages, hash|
-          abstract_pages.each do |abstract_page|
-            abstract_page[1].each do |abstract_platform|
-              hash.merge!(abstract_platform)
-            end
-          end
-        end
-        
-        roles_config['pages'].each_with_index do |pages, i_1|
-          pages.each_pair do |key_1, page|
-            page.each_with_index do |platform, i_2|
-              platform.each_pair do |key_2, value|
-                abstract_platform_key = if value.to_h['extend_abstract']
-                  value.to_h['extend_abstract']
-                elsif abstract_platform_config[key_2]
-                  key_2
-                end
-                next unless abstract_platform_key
-                roles_config['pages'][i_1][key_1][i_2][key_2] = abstract_platform_config[abstract_platform_key].deep_merge(value.to_h)
-              end
-            end
-          end
-        end
-      end
-      
-      @config = Bhf::Settings.new(roles_config)
-    end
-
-
-    def get_config_array(array, dir)
-      array.each_with_object({'pages' => []}) do |r, account_roles|
-        pages = load_yml("#{dir}/#{r}")['pages']
-        account_roles['pages'] += pages if pages
-      end
-    end
-    
-    def roles_yml(roles = nil)
-      area_dir = "/#{params[:bhf_area]}" if params[:bhf_area].present?
-      if roles.is_a?(String)
-        load_yml("#{area_dir}/#{roles}")
-      elsif roles.is_a?(Array)
-        files = get_config_array(roles, area_dir)
-
-        # find the same pages and merge them
-        merged_files = {'pages' => []}
-        files['pages'].each do |pages|
-          merged = false
-          pages.each_pair do |key, page|
-            merged_files['pages'].each do |m_page|
-              if m_page.include?(key)
-                merged = true
-                m_page[key] = m_page[key] + page
-              end
-            end
-          end
-          if !merged
-            merged_files['pages'] << pages
-          end
-        end
-        merged_files
-      else
-        load_yml
-      end
-    end
-
-    def load_yml(suffix = nil)
-      YAML::load(IO.read("config/bhf#{suffix}.yml"))
-    end
-
     def get_account_roles(area = nil)
       return unless current_account
       
@@ -125,6 +51,9 @@ class Bhf::ApplicationController < ActionController::Base
       end
     end
 
+    def load_config
+      @config = Bhf::ConfigParser::parse(get_account_roles(params[:bhf_area]), params[:bhf_area])
+    end
 
     def set_title
       @app_title = Rails.application.class.to_s.split('::').first
