80bfa8c85ad9698093eca37200bdf834d1c614f1
Anton Pawlik <anton.pawlik@gmail.com>
2014-06-02 11:27:39 +0200

    clean up

--- lib/bhf.rb
+++ lib/bhf.rb
@@ -1,46 +1,39 @@
-def d(var)
-  raise var.inspect.split(', ').join(",\n")
-end
-require 'haml'
-require 'kaminari'
-
 module Bhf
   class Engine < Rails::Engine
     
     isolate_namespace Bhf
     
-    # config.bhf defaults
-    config.bhf = OpenStruct.new
-    config.bhf.page_title = nil
-    config.bhf.on_login_fail = :root_url
-    config.bhf.logout_path = :logout_path
-    config.bhf.session_auth_name = :is_admin
-    config.bhf.session_account_id = :admin_account_id
-    config.bhf.account_model = 'User'
-    config.bhf.account_model_find_method = 'find'
-    config.bhf.css = []
-    config.bhf.js = []
-    config.bhf.abstract_config = []
+    config.bhf = OpenStruct.new(
+      on_login_fail: :root_url,
+      logout_path: :logout_path,
+      session_auth_name: :is_admin,
+      session_account_id: :admin_account_id,
+      account_model: 'User',
+      account_model_find_method: 'find',
+      css: ['bhf/application'],
+      js: ['bhf/application'],
+      abstract_config: []
+    )
     
-    # config.bhf.config = YAML::load(IO.read('config/bhf.yml'))
-    
-    initializer 'bhf.action_controller' do |app|
+    initializer 'bhf.helper' do |app|
       ActiveSupport.on_load :action_controller do
         helper Bhf::FrontendHelper
       end
     end
     
-    initializer 'bhf.hooks' do
-      if defined?(::ActiveRecord)
-        ::ActiveRecord::Base.send :include, Bhf::ActiveRecord::Object
+    initializer 'bhf.model_hooks' do
+      ActiveSupport.on_load :active_record do
+        include Bhf::ActiveRecord::Object
       end
-      if defined?(::Mongoid)
-        ::Mongoid::Document.send :include, Bhf::Mongoid::Document
+      ActiveSupport.on_load :mongoid do
+        include Bhf::Mongoid::Document
       end
     end
   end
 end
 
+require 'haml'
+require 'kaminari'
 require 'bhf/active_record/active_record'
 require 'bhf/mongoid/document'
 require 'bhf/data'
@@ -49,5 +42,3 @@ require 'bhf/config_parser'
 require 'bhf/settings'
 require 'bhf/pagination'
 require 'bhf/form'
-
-
