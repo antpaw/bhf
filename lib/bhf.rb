ef1f08e79c8e713da9145b3b045f35b5d0501433
Malo Skrylevo <majioa@yandex.ru>
2015-10-01 23:37:59 +0300

    Image tag now is shewn as a thumnnail, and when a user turn mouse curson over it, the popup image has being shown up.

--- lib/bhf.rb
+++ lib/bhf.rb
@@ -41,7 +41,10 @@ module Bhf
     config_accessor(:account_model)             { 'User'              }
     config_accessor(:account_model_find_method) { 'find'              }
     config_accessor(:css)                       { ['bhf/application'] }
-    config_accessor(:js)                        { ['bhf/application'] }
+    config_accessor(:js)                        { [
+       'http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js',
+       'bhf/application'
+    ] }
     config_accessor(:abstract_settings)         { []                  }
     config_accessor(:paperclip_image_types)     {
       ['image/jpeg', 'image/pjpeg', 'image/jpg', 'image/png',
