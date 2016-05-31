a6d692f5c83cb3d714dcf331eec5b5311b42b11e
Anton Pawlik <anton.pawlik@gmail.com>
2016-05-31 22:00:36 +0200

    removed depricated find_assets method

--- app/helpers/bhf/application_helper.rb
+++ app/helpers/bhf/application_helper.rb
@@ -35,5 +35,13 @@ module Bhf
       Bhf.configuration.paperclip_image_types.include?(type)
     end
 
+    def asset_exist?(path)
+      if Rails.configuration.assets.compile
+        Rails.application.precompiled_assets.include? path
+      else
+        Rails.application.assets_manifest.assets[path].present?
+      end
+    end
+
   end
 end
